84cb19dbd99aa977b4943bcf52648d2204dd5d10
Michaux Kelley <michauxkelley@gmail.com>
2018-07-10 20:21:41 -0400

    Update config settings for editor and excludesfile

--- bootstrap_gitconfig.sh
+++ bootstrap_gitconfig.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+# Must source zshrc before executing this file
+
 ln -s $DOTS/bootstrap_gitconfig.sh .bootstrap_gitconfig.sh
 # aliases managed by git .aliases file and zsh git plugin
 
@@ -12,8 +14,8 @@ git config --global branch.autosetupmerge true
 git config --global push.default tracking
 git config --global rerere.enabled true
 
-git config --global core.editor vim
-git config --global core.ignore $DOTS/gitignores/global.gitignore
+git config --global core.editor code
+git config --global core.excludesfile ~/.gitignore
 git config --global core.whitespace trailing-space,space-before-tab
 
 git config --global color.ui auto
