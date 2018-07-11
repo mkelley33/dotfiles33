88a8609b46b237c77bee0d5ba4f2901911cc3dd9
Michaux Kelley <michauxkelley@gmail.com>
2018-07-10 20:19:30 -0400

    Update file to make symlinks to dot files

--- makesymlinks.bash
+++ makesymlinks.bash
@@ -3,15 +3,16 @@
 # This file is pretty much what you see here with some minor modifications:
 # https://raw.github.com/michaeljsmalley/dotfiles/master/makesymlinks.sh
 
-dots=$HOME/$DOTFILES
 old_dots=$HOME/old_dots
-
+DOTS=$HOME/projects/mk/git/me/dotfiles33
 # files to symlink in $HOME
-files="vimrc aliases ctags gemrc inputrc pryrc rvmrc.global tmux.conf zlogin zsh_functions zshrc"
+files="vimrc aliases ackrc ctags gemrc inputrc pryrc rvmrc.global tmux.conf zlogin zsh_functions zshrc"
 mkdir -p $old_dots && cd $old_dots
 for file in $files; do
   echo "Moving any existing dotfiles from ~ to $old_dots"
   mv $HOME/.$file $old_dots/
   echo "Creating symlink to $file in home directory."
-  ln -s $DOTFILES/$file $HOME/.$file
+  ln -s $DOTS/$file $HOME/.$file
 done
+
+ln -s $DOTS/gitignores/global.gitignore $HOME/.gitignore
